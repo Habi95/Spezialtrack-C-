a42524f4ac92fd1f6d3112b3eb417f6fcdaf7586
Habi95 <Habi95@users.noreply.github.com>
2020-06-03 12:07:17 +0200

    FileReader/Writer Finished

---
+++ C#/PersonFileReader/PersonFileReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using PersonFileReader.model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PersonFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personsList = new List<Person>();
+            string []  personWithoutJob = System.IO.File.ReadAllLines("C:\\Users\\DCV\\Desktop\\HelloWorld\\Spezialtrack-C-\\C#\\persons.txt");
+
+            foreach (string personinfo in personWithoutJob)
+            {
+                string[] perColumn = personinfo.Split(";");
+                int.TryParse(perColumn[1], out int age);
+                Person person = new Person()
+                {
+                    Name = perColumn[0],
+                    Age = age,
+                    LivingPlace = perColumn[2],
+                    Job = perColumn[0] == "Hans" || perColumn[0] == "Helga" ? "Coder" : "Officer"
+                };
+                personsList.Add(person);
+            }
+            List<string> personsListWithJob = new List<string>();
+            foreach (Person person in personsList)
+            {
+                personsListWithJob.Add($"{person.Name};{person.Age};{person.LivingPlace};{person.Job}");
+            }
+            File.WriteAllLines("C:\\Users\\DCV\\Desktop\\HelloWorld\\Spezialtrack-C-\\C#\\peronwithJob.txt", personsListWithJob);
+        }
+    }
+}
---
+++ C#/PersonFileReader/PersonFileReader/model/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonFileReader.model
+{
+    class Person
+    {
+        /// <summary>
+    /// firstname
+    /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// current age
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// hometown
+        /// </summary>
+        public string LivingPlace { get; set; }
+        /// <summary>
+        /// Job of Person
+        /// </summary>
+        public string Job { get; set; }
+    }
+}
