0120c854fbf5fb9245df3c7400b404371b560cfb
Habi95 <Habi95@users.noreply.github.com>
2020-06-05 10:03:22 +0200

    first extensions

---
+++ C#/Util/Util/Extensions/ListExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Util.Extensions
+{
+    public static class ListExtension
+    {
+        public static string ToConsole<T>(this List<T> list)
+        {
+            string output = string.Empty;
+
+            foreach (var item in list)
+            {
+                if (!string.IsNullOrEmpty(output))
+                {
+                    output = $"{output}\n";
+                }
+                output = output + item.ToString();
+            }
+            return output;
+        }
+    }
+}
---
+++ C#/Util/Util/Extensions/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util.Extensions
+{
+    public static class StringExtension
+    {
+     
+       public static T? ConvertType<T>(this string input) where T : struct
+        {
+            try
+            {
+                return (T)Convert.ChangeType(input, typeof(T)); 
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return null;
+        }
+    }
+    
+}
